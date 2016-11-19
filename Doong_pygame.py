a0e51dce4d589392e616174ac092b317286e4e82
Susan Doong <Susan Doong>
2016-11-19 16:08:50 -0500

    First commit

---
+++ Doong_pygame.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+import sys
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.update()
+pygame.display.get_surface()
+
+screen.blit(sourceSurface, destinationRect, optionalSourceRect)
