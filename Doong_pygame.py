bf12350184c08a4f65de6bdc9175c0dc3edc4f30
Susan Doong <Susan Doong>
2016-11-20 10:35:00 -0500

    Adds block movement and boundaries

--- Doong_pygame.py
+++ Doong_pygame.py
@@ -1,13 +1,77 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random
-import sys
-import pygame
-pygame.init()
+import random, sys, time, math, pygame
+pygame.init();
+from pygame.locals import *
+import time
 
-screen = pygame.display.set_mode((width, height))
-pygame.display.update()
-pygame.display.get_surface()
+#defining colors
+white = (255,255,255)
+black = (0,0,0)
+red = (255, 0, 0)
+green = (34, 139, 34)
+blue = (0, 0, 255)
 
-screen.blit(sourceSurface, destinationRect, optionalSourceRect)
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Doong Pygame")
+
+def main():
+	fps = 20 # frames per second to update the screen
+	width = 800
+	height = 600
+
+	start_x = width/2
+	start_y = height/2
+	x_change = 0
+	y_change = 0
+	movement = 10
+
+	gameDisplay = pygame.display.set_mode((width,height))
+
+	gameExit = False
+	while not gameExit:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				gameExit = True
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					x_change = -movement
+					y_change = 0
+				elif event.key == pygame.K_RIGHT:
+					x_change = movement
+					y_change = 0
+				elif event.key == pygame.K_UP:
+					y_change = -movement
+					x_change = 0
+				elif event.key == pygame.K_DOWN:
+					y_change = movement
+					x_change = 0
+
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT or pygame.K_RIGHT or pygame.K_UP or pygame.K_DOWN:
+					x_change = 0
+					y_change = 0
+
+		f = pygame.font.Font(None, 32)
+		if start_x >= width or start_x <= 0 or start_y >= height or start_y <= 0:
+			gameExit = True
+			label = f.render("Game over!", True, red)
+			gameDisplay.blit(label, [width/2, height/2])
+			pygame.display.update()
+
+		start_x += x_change
+		start_y += y_change
+
+		gameDisplay.fill(green)
+		pygame.draw.rect(gameDisplay, blue, [start_x, start_y, 15, 15])
+		#pygame.draw.rect(gameDisplay, red, [100,200, 15, 15])
+		pygame.display.update()
+
+		clock.tick(fps)
+
+	pygame.quit()
+	quit()
+
+main()
