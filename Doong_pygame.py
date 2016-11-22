24521f0d0a075a17c9264e31a03e7e7d1d7e188a
Susan Doong <Susan Doong>
2016-11-22 08:29:13 -0500

    Monsters randomly differently sized

--- Doong_pygame.py
+++ Doong_pygame.py
@@ -1,9 +1,18 @@
 from pygame import *
 from pygame.sprite import *
 from random import *
+import random 
 
 DELAY = 1000;            #Seed a timer to move sprite
+white = (255,255,255)
+black = (0,0,0)
+red = (255, 0, 0)
 green = (34, 139, 34)
+blue = (0, 0, 255)
+
+width = 700
+height = 500
+
 bgcolor = green   #Color taken from background of sprite
 
 class Monster(Sprite):
@@ -11,77 +20,81 @@ class Monster(Sprite):
         Sprite.__init__(self) #importan
         self.image = image.load("monster.png").convert_alpha()
         self.rect = self.image.get_rect()
+        self.size = self.image.get_size()
+        size_inc = random.uniform(0, 2)
+        self.image = pygame.transform.scale(self.image, (int(self.size[0]*size_inc), int(self.size[1]*size_inc)))
 
     # move gold to a new random location
     def move(self):
-        randX = randint(0, 600)
-        randY = randint(0, 400)
-        self.rect.center = (randX,randY)
+        current_randX = randint(0, 650)
+        current_randY = randint(0, 450)
+        self.rect.center = (current_randX, current_randY)
 
-class Shovel(Sprite):
+class Player(Sprite):
     def __init__(self):
         Sprite.__init__(self)
-        self.image = image.load("shovel.gif").convert()
+        self.image = image.load("player.png").convert_alpha()
         self.rect = self.image.get_rect()
+        self.x_change = width/2
+        self.y_change = height/2
 
-    # Di shovel/cursor collide the gold?
-    def hit(self, target):
-        return self.rect.colliderect(target)
+    def move(self):
+        key = pygame.key.get_pressed()
+        dist = 0.5
+  
+        if key[pygame.K_DOWN]: # down key
+            self.y_change += dist # move down
+        elif key[pygame.K_UP]: # up key
+            self.y_change -= dist # move up
+        if key[pygame.K_RIGHT]: # right key
+            self.x_change += dist # move right
+        elif key[pygame.K_LEFT]: # left key
+            self.x_change -= dist # move left
 
-    #The shovel sprite will move with the mousepointer
-    def update(self):
-        self.rect.center = mouse.get_pos()
+    def draw(self, surface):
+        surface.blit(self.image, (self.x_change, self.y_change))
 
-
-#main
+movement = 10
 init()
 
-screen = display.set_mode((640, 480))
+screen = display.set_mode((width, height))
 display.set_caption('Doong Pygame')
 
-# hide the mouse cursor so we only see shovel
-mouse.set_visible(False)
-
-f = font.Font(None, 25)
-
-# create the mole and shovel using the constructors
 sprites2 = pygame.sprite.Group()
 
-for x in range(randint(5, 15)):
+for x in range(randint(10, 20)):
     monster = Monster()
     sprites2.add(monster)
 
+player = Player()
 # creates a group of sprites so all can be updated at once
 sprites = RenderPlain(sprites2)
 
 hits = 0
 time.set_timer(USEREVENT + 1, DELAY)
 
+movement = 10
+start_x = 0
+start_y = 0
+x_change = 0
+y_change = 0
+
+gameExit = False
 # loop until user quits
-while True:
-    e = event.poll()
-    if e.type == QUIT:
-        quit()
-        break
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            quit()
+            break
 
-    elif e.type == MOUSEBUTTONDOWN:
-        if shovel.hit(monster):
-            mixer.Sound("cha-ching.wav").play()
-            gold.move()
-            hits += 1
-
-            # reset timer
-            time.set_timer(USEREVENT + 1, DELAY)
-            
-    elif e.type == USEREVENT + 1: # TIME has passed
-        for y in sprites2:
-            y.move()
-
+        elif event.type == USEREVENT + 1: 
+            for y in sprites2:
+                y.move()
     # refill background color so that we can paint sprites in new locations
     screen.fill(bgcolor)
-    t = f.render("Jackpot = " + str(hits), False, (0,0,0))
-    screen.blit(t, (320, 0))        # draw text to screen.  Can you move it?
-
+    player.move()
+    player.draw(screen)
+    
     # update and redraw sprites
     sprites.update()
     sprites.draw(screen)
