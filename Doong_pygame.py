d77f9f250bfbafc716d447efe45aa71113bedbae
Susan Doong <Susan Doong>
2016-11-28 13:10:31 -0500

    Adds difficulty levels and sound

--- Doong_pygame.py
+++ Doong_pygame.py
@@ -11,7 +11,7 @@ import random
 import sys
 
 #constants for program
-DELAY = 1000;            #Seed a timer to move sprite
+DELAY = 1000; #Seed a timer to move sprite
 
 #colors
 WHITE = (255,255,255)
@@ -34,19 +34,21 @@ cookie_sprites = pygame.sprite.Group()
 player_sprite = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
+pygame.init()
+
 class Cookie(Sprite):
-    def __init__(self):
+    def __init__(self, cookie_max): #cookie_max represents the maximize size increase the cookie can have. Limits size.
         Sprite.__init__(self) 
         self.image = image.load("cookie.png").convert_alpha()
         self.size = self.image.get_size()
-        size_inc = random.uniform(0.25, 2.5)
+        size_inc = random.uniform(0.35, cookie_max)
         #using transform.scale to make each monster a different size
         self.image = pygame.transform.scale(self.image, (int(self.size[0]*size_inc), int(self.size[1]*size_inc)))
         self.rect = self.image.get_rect()
         self.rect.x = randint(50, 750)
         self.rect.y = randint(50, 550)
         
-    def move(self):
+    def move(self): #cookie's random movements
         self.move_x = randint(-50, 50)
         self.move_y = randint(-50, 50)
         self.rect.x += self.move_x
@@ -86,14 +88,13 @@ class Player(Sprite):
 
     def inc_size(self): #increases size of player when it eats smaller monsters
         self.size = self.image.get_size()
-        if self.size[0] < 50 and self.size[1] < 63:
-            self.image = pygame.transform.scale(self.image, (int(self.size[0]+2), int(self.size[1]+2))) #visually increase size
-            self.rect.inflate_ip(2, 2) #actually increase size
+        self.image = pygame.transform.scale(self.image, (int(self.size[0]+2), int(self.size[1]+2))) #visually increase size
+        self.rect.inflate_ip(2, 2) #actually increase size
         #self.size = self.image.get_size()
 
-def make_cookies():
-    for x in range(randint(20, 30)): #makes between 20 to 30 random cookies
-        cookie = Cookie()
+def make_cookies(minc, maxc, cookie_max):
+    for x in range(randint(minc, maxc)): #makes between minc and maxc random cookies
+        cookie = Cookie(cookie_max)
         cookie_sprites.add(cookie)
         all_sprites.add(cookie)
 
@@ -104,14 +105,21 @@ def message(msg, color, screencolor, screen, width, height):
     screen.blit(label, (width, height))
     pygame.display.flip()
 
+screen = display.set_mode((width, height))
+
+#Intro screens and instructions
+message("Monster Eats Cookie Game", BLACK, WHITE, screen, 250, 300) #display screen before game starts
+pygame.time.delay(2000)
+message("Eat as many cookies as you can but don't get squashed by bigger cookies!", BLACK, WHITE, screen, 30, 325)
+pygame.time.delay(3000)
+message("You can only eat smaller cookies, but you get bigger with each cookie you eat!", BLACK, WHITE, screen, 20, 325)
+pygame.time.delay(3000) #delay game start
+
 def main():
+    score = 0
     movement = 10
-    init()
 
-    screen = display.set_mode((width, height))
     display.set_caption('Doong Pygame')
-
-    make_cookies()
 
     player = Player()
 
@@ -123,8 +131,36 @@ def main():
     gameExit = False
     gameOver = False
 
-    message("Monster Eats Cookie Game", BLACK, WHITE, screen, 250, 300) #display screen before game starts
-    pygame.time.delay(2000) #delay game start
+    minc = 0
+    maxc = 0
+
+    intro = True 
+
+    #intro screen
+    while intro:
+        message("Game Level: Press 1 for Easy, 2 for Medium, 3 for Hard", BLACK, WHITE, screen, 130, 300) #allows player to pick level
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                #assigns number of cookies and cookie sizes based on difficulty level
+                if event.key == pygame.K_1: 
+                    minc = 8
+                    maxc = 12
+                    cookie_max = 1.4
+                    intro = False
+                if event.key == pygame.K_2: 
+                    minc = 15
+                    maxc = 20
+                    cookie_max = 2
+                    intro = False
+                if event.key == pygame.K_3: 
+                    minc = 23
+                    maxc = 28
+                    cookie_max = 2.3
+                    intro = False
+
+    make_cookies(minc, maxc, cookie_max)
 
     # loop until user quits ------------- main --------------------- #
     while not gameExit:
@@ -143,39 +179,44 @@ def main():
         collide_list = pygame.sprite.spritecollide(player, cookie_sprites, remove) #collision detection
 
         for hit in collide_list:
-            print ("monster size ", hit.rect.size)
-            print (len(cookie_sprites))
+            score += 1
+            mixer.Sound("bitesound.wav").play()
 
             if player.rect.size < hit.rect.size: #checks if player is smaller than monster
-                message("Game Over. You got squashed by a giant cookie!", RED, BLACK, screen, 190, 300)
+                mixer.Sound("lost.wav").play()
+                message("Game Over. You got squashed by a giant cookie!", RED, BLACK, screen, 170, 300)
                 pygame.time.delay(2000)
                 gameOver = True
 
             else:
                 player.inc_size()
-                print (player.rect.size)
-
+                
                 if len(cookie_sprites) == 0: #if no more cookies, then player wins
                     screen.fill(WHITE)
+                    mixer.Sound("tada.wav").play()
                     message("You win! You ate all the cookies!", GREEN, WHITE, screen, 225, 300)
                     pygame.time.delay(2000)
                     gameOver = True
 
         while gameOver == True:
             screen.fill(BLACK)
-            message("Q to quit or P to play again.", RED, BLACK, screen, 275, 300)
+            message("Q to quit or P to play again.", RED, BLACK, screen, 250, 300)
             pygame.display.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q: #if key q is pressed
+                    if event.key == pygame.K_q: #if key q is pressed, exit game
                         exit()
-                    if event.key == pygame.K_p: #if key p is pressed
+                    if event.key == pygame.K_p: #if key p is pressed, restart game
                         cookie_sprites.empty() #reset cookie_sprites list
                         main()
             
-        # refill background color so that we can paint sprites in new locations
+        # refill background color 
         screen.fill(bgcolor)
+
+        myfont2 = font.Font(None, 25)
+        t = myfont2.render("Cookies Eaten = " + str(score), False, WHITE) #score counter
+        screen.blit(t, (320, 0))  
 
         player.update()
         player.draw(screen)
@@ -188,4 +229,4 @@ def main():
 
         clock.tick(FPS) #limits to 60 frames per second
 
-main()
+main() #calls main function to run the game
